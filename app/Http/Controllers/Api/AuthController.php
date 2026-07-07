2bd1ca83a6b7db82b81084b74bf993dbd6aab8d1
AlbarFahrezi <albarfahrezi7@gmail.com>
2026-07-07 13:02:44 +0700

    Initial commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        $validated = $request->validated();
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => $validated['password'],
+            'role' => 'user',
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Register berhasil',
+            'data' => [
+                'user' => new UserResource($user),
+                'token' => $token,
+            ]
+        ], 201);
+    }
+}
